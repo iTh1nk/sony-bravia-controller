c9e3923dbdfaa65001f13f7f2fbcfe9cf7da7301
iTh1nk <fnchao@hotmail.com>
2020-12-09 22:15:36 -0800

    setup page layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import Container from "../components/Container";
 import Toggle from "../components/Toggle";
 import styled from "styled-components";
 import tw from "twin.macro";
+import { useEffect } from "react";
+import Axios from "axios";
 
 const Title = styled.div(() => [
   tw`text-purple-800 font-semibold text-lg mr-2`,
@@ -10,8 +12,43 @@ const SubTitle = styled.div(() => [tw`text-purple-800`]);
 const Button = styled.button(() => [tw`mx-2 my-1 rounded-sm px-1`]);
 
 export default function IndexPage() {
+  // useEffect(() => {
+  //   Axios.post(
+  //     "http://10.0.0.98/sony/appControl",
+  //     { method: "getApplicationList", id: 60, params: [], version: "1.0" },
+  //     { headers: { "X-Auth-PSK": "mac" } }
+  //   )
+  //     .then((resp) => {
+  //       console.log(resp);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err, err.response);
+  //     });
+  // });
+  const testButton = () => {
+    Axios.post(
+      "http://10.0.0.98/sony/appControl",
+      { method: "getApplicationList", id: 60, params: [], version: "1.0" },
+      { headers: { "X-Auth-PSK": "mac" } }
+    )
+      .then((resp) => {
+        console.clear();
+        console.log(resp.data.result[0]);
+        alert(JSON.stringify(resp.data.result[0]));
+      })
+      .catch((err) => {
+        console.log(err, err.response);
+      });
+  };
+
   return (
     <Container>
+      <button
+        className="text-center w-full mt-5 border border-purple-300"
+        onClick={() => testButton()}
+      >
+        TEST
+      </button>
       <div className="p-6">
         <div className="flex flex-row justify-start items-center">
           <Title>
@@ -39,17 +76,17 @@ export default function IndexPage() {
             <SubTitle>YouTube</SubTitle>
           </Button>
         </div>
-        <div>
+        <div className="mt-5">
           <div className="mb-2">
             <Title>- Volume</Title>
           </div>
         </div>
-        <div>
+        <div className="mt-5">
           <div className="mb-2">
             <Title>- Input</Title>
           </div>
         </div>
-        <div>
+        <div className="mt-5">
           <div className="mb-2">
             <Title>- Panel</Title>
           </div>
