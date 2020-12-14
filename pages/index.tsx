c80d96339a30fe0f36c740f07b2cd6e98f177ba0
iTh1nk <fnchao@hotmail.com>
2020-12-13 21:45:09 -0800

    config volume func & add panel layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -256,7 +256,6 @@ export default function IndexPage() {
       });
   };
   const muteCtl = () => {
-    dispatch({ type: "isLoading", payload: true });
     Axios.post(
       "http://10.0.0.98/sony/audio",
       {
